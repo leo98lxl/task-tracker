62cf955519235e5823b3d34235c00fd27af418bf
leo98lxl <leo.leksell@live.se>
2026-06-16 11:33:07 +0200

    task tracker checklist

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,24 @@
-console.log("Hello TypeScript!");
-console.log("Hello again TypeScript!");
+const task1Name = "Learn TypeScript";
+const task1Priority = 4;
+const task1Completed = false;
+const task2Name = "Walk my dog";
+const task2Priority = 5;
+const task2Completed = false;
+const task3Name = "Brush my teeth";
+const task3Priority = 3;
+const task3Completed = true;
+const completedTasks = 1;
+const totalTasks = 3;
+const completionRate = completedTasks / totalTasks * 100;
+console.log(`Task: ${task1Name} 
+            Priority: ${task1Priority} 
+            Completed: ${task1Completed}`);
+console.log(`Task: ${task2Name} 
+            Priority: ${task2Priority} 
+            Completed: ${task2Completed}`);
+console.log(`Task: ${task3Name} 
+            Priority: ${task3Priority} 
+            Completed: ${task3Completed}`);
+console.log(`Completion rate: ${completionRate}`);
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,29 @@
-console.log("Hello TypeScript!");
-console.log("Hello again TypeScript!");
+const task1Name: string = "Learn TypeScript";
+const task1Priority: number = 4;
+const task1Completed: boolean = false;
+
+const task2Name: string = "Walk my dog";
+const task2Priority: number = 5;
+const task2Completed: boolean = false;
+
+const task3Name: string = "Brush my teeth";
+const task3Priority: number = 3;
+const task3Completed: boolean = true;
+
+const completedTasks: number = 1;
+const totalTasks: number = 3;
+const completionRate: number = completedTasks / totalTasks * 100;
+
+console.log(`Task: ${task1Name} 
+            Priority: ${task1Priority} 
+            Completed: ${task1Completed}`);
+
+console.log(`Task: ${task2Name} 
+            Priority: ${task2Priority} 
+            Completed: ${task2Completed}`);
+
+console.log(`Task: ${task3Name} 
+            Priority: ${task3Priority} 
+            Completed: ${task3Completed}`);
+
+console.log(`Completion rate: ${completionRate}`);
